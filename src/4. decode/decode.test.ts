0654e01d3064fb5f41b9911a3ba1ea0c0eebd44e
alnavarrop99 <anavarro.develop99@gmail.com>
2024-01-24 12:37:22 -0500

    feat(excercise/4): setup decode and reverInner functions

--- src/4. decode/decode.test.ts
+++ src/4. decode/decode.test.ts
@@ -1,12 +1,13 @@
 /**
  * En el taller de Santa 🎅, algunos mensajes navideños han sido escritos de manera peculiar: las letras dentro de los paréntesis deben ser leídas al revés
- * Santa necesita que estos mensajes estén correctamente formateados. Tu tarea es escribir una función que tome una cadena de texto y revierta los caracteres dentro de cada par de paréntesis, eliminando los paréntesis en el mensaje final.
- * Eso sí, ten en cuenta que pueden existir paréntesis anidados, por lo que debes invertir los caracteres en el orden correcto.
+ * Santa necesita que estos mensajes estén correctamente formateados. Tu tarea es escribir una función que tome una cadena de texto y revierta los caracteres
+ * dentro de cada par de paréntesis, eliminando los paréntesis en el mensaje final.Eso sí, ten en cuenta que pueden existir paréntesis anidados, por lo que debes
+ * invertir los caracteres en el orden correcto.
  *
  * Notas:
- * Las cadenas de entrada siempre estarán bien formadas con paréntesis que coinciden correctamente, no necesitas validarlos.
- * En el mensaje final no deben quedar paréntesis.
- * El nivel máximo de anidamiento es 2.
+ * - Las cadenas de entrada siempre estarán bien formadas con paréntesis que coinciden correctamente, no necesitas validarlos.
+ * - En el mensaje final no deben quedar paréntesis.
+ * - El nivel máximo de anidamiento es 2.
  */
 
 /**
@@ -26,15 +27,26 @@ console.log(c) // santaclaus
 ```
  * */
 
-import { decode } from './decode'
+import { decode, revertInner } from './decode'
+
+describe('TDD: revertInner test', () => {
+  test('Scenario 1: Sin Anidacion', () => {
+    expect(revertInner('(odnum)')).toBe('mundo')
+  })
+  test('Scenario 2: Anidacion profunda', () => {
+    expect(revertInner('sa(u(cla)atn)s')).toBe('santaclaus')
+  })
+})
+
 describe('TDD: decode test', () => {
-  test('Sceneario 1', () => {
-    expect(decode('')).toBe('')
+  test('Scenario 1', () => {
+    expect(decode('hola (odnum)')).toBe('hola mundo')
   })
-  test('Sceneario 2', () => {
-    expect(decode('')).toBe('')
+  test('Scenario 2', () => {
+    expect(decode('(olleh) (dlrow)!')).toBe('hello world!')
   })
-  test('Sceneario 3', () => {
-    expect(decode('')).toBe('')
+  test('Scenario 3', () => {
+    expect(decode('sa(u(cla)atn)s')).toBe('santaclaus')
+    expect(decode('sa(u(cla)atn)s (apap)')).toBe('santaclaus papa')
   })
 })
--- src/4. decode/decode.ts
+++ src/4. decode/decode.ts
@@ -1,4 +1,25 @@
+export const revertInner = (message: string, acc?: string): string => {
+  if (!message.match(/(\(.*\))/g)) {
+    return message
+  } else {
+    const textInner = message.match(/(\(.*\))/g)?.join('')
+    const res = message.split(textInner ?? '').copyWithin(0, 0)
+    return (
+      res[0] +
+      revertInner(textInner?.slice(1, textInner.length - 1) ?? '')
+        .split('')
+        .reverse()
+        .join('') +
+      res[res.length - 1]
+    )
+  }
+}
+
 export const decode = (message: string): string => {
-  // Code here
-  return ''
+  const spaceSeparator = message.split(' ')
+  const res: string[] = []
+  for (const val of spaceSeparator) {
+    res.push(revertInner(val))
+  }
+  return res.join(' ')
 }
