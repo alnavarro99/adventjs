5472e647f1d7782d14fd5ce859da2c67e6d91149
alnavarrop99 <anavarro.develop99@gmail.com>
2024-01-26 20:35:02 -0500

    feat(excercise/10): setup adjustLight and createLights functions Tags: #fails #adventjs

--- src/Reto #10: Crea tu propio árbol de navidad/index.ts
+++ src/Reto #10: Crea tu propio árbol de navidad/index.ts
@@ -1,3 +1,25 @@
 export const createChristmasTree = (ornaments: string, height: number): string => {
-  return '|'
+  if (height > 0) {
+    const tree = Array(height).fill('')
+    const list = ornaments.repeat(2 * (height + ornaments.length)).slice(0, sum(height))
+    return (
+      [
+        ...tree.map(
+          (_, i, array) =>
+            ' '.repeat(array.length - 1 - i) +
+            list
+              .slice(sum(i), sum(i + 1))
+              .replace(/./g, '$& ')
+              .trim()
+        ),
+        ' '.repeat(tree.length - 1) + '|',
+      ].join('\n') + '\n'
+    )
+  } else return '|\n'
+}
+
+export const sum = (n: number): number => {
+  if (n === 1) return 1
+  if (n <= 0) return 0
+  return n + sum(n - 1)
 }
--- src/Reto #10: Crea tu propio árbol de navidad/test.ts
+++ src/Reto #10: Crea tu propio árbol de navidad/test.ts
@@ -5,12 +5,62 @@
  * - Debemos devolver un string multilínea con el árbol de Navidad formado con los adornos, la altura indicada más una última línea con el tronco formado por el carácter | en el centro y, finalmente, un salto de línea \n.
  */
 
-import { createChristmasTree } from '.'
+// asdasd
+import { createChristmasTree, sum } from '.'
 
-describe('TDD: create christmas tree -> primary cases', () => {
-  test('SC: primary cases', () => {
+describe('TDD: create christmas tree', () => {
+  test('SC1: primary cases', () => {
     expect(createChristmasTree('123', 4)).toBe(result1)
     expect(createChristmasTree('*@o', 3)).toBe(result2)
+    expect(createChristmasTree('X', 3)).toBe(result3)
+    expect(createChristmasTree('*', 3)).toBe('  *\n * *\n* * *\n  |\n')
+    expect(createChristmasTree('ABC123', 9)).toBe(result4)
+  })
+  test('SC2: especial cases cases', () => {
+    expect(createChristmasTree('*', 0)).toBe('|\n')
+    expect(createChristmasTree('*', -10)).toBe('|\n')
+  })
+})
+
+describe('TDD: create generator ornament list', () => {
+  const generator = jest.fn((length: number, text: string): string => text.repeat(2 * (length + text.length)).slice(0, sum(length)))
+  test('SC1: primary cases -> odd and even ornament and lengths', () => {
+    expect(generator(4, '123')).toBe('1231231231')
+    expect(generator(4, '1234')).toBe('1234123412')
+    expect(generator(4, 'ABC')).toBe('ABCABCABCA')
+    expect(generator(4, 'ABCD')).toBe('ABCDABCDAB')
+    expect(generator(3, 'ABC')).toBe('ABCABC')
+    expect(generator(3, 'ABCD')).toBe('ABCDAB')
+    expect(generator(3, '123')).toBe('123123')
+    expect(generator(3, '1234')).toBe('123412')
+    expect(generator(2, 'ABC')).toBe('ABC')
+    expect(generator(2, 'ABCD')).toBe('ABC')
+    expect(generator(2, '123')).toBe('123')
+    expect(generator(2, '1234')).toBe('123')
+  })
+  test("SC2: especial cases -> 0, 1, 2 length, '', ' ', $%^ ornament  ", () => {
+    expect(generator(0, '')).toBe('')
+    expect(generator(0, 'ABC')).toBe('')
+    expect(generator(1, 'A')).toBe('A')
+    expect(generator(3, 'B')).toBe('BBBBBB')
+    expect(generator(1, 'ABCD')).toBe('A')
+    expect(generator(2, 'AB')).toBe('ABA')
+    expect(generator(2, 'ABCD')).toBe('ABC')
+    expect(generator(3, '')).toBe('')
+    expect(generator(3, '$%^')).toBe('$%^$%^')
+  })
+})
+
+describe('TDD: sum', () => {
+  test('SC1: primary cases', () => {
+    expect(sum(3)).toBe(6)
+    expect(sum(4)).toBe(10)
+    expect(sum(5)).toBe(15)
+  })
+  test('SC2: especial cases', () => {
+    expect(sum(0)).toBe(0)
+    expect(sum(1)).toBe(1)
+    expect(sum(-10)).toBe(0)
   })
 })
 
@@ -25,4 +75,22 @@ const result2 = `  *
  @ o
 * @ o
   |
+`
+
+const result3 = `  X
+ X X
+X X X
+  |
+`
+
+const result4 = `        A
+       B C
+      1 2 3
+     A B C 1
+    2 3 A B C
+   1 2 3 A B C
+  1 2 3 A B C 1
+ 2 3 A B C 1 2 3
+A B C 1 2 3 A B C
+        |
 `
